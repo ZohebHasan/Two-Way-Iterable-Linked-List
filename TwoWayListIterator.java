a445ad7c833490d2151e7e55d87a8b8e437115b4
Zoheb Hasan <106490884+ZohebHasan@users.noreply.github.com>
2022-10-30 16:55:15 -0400

    Update TwoWayListIterator.java

--- TwoWayListIterator.java
+++ TwoWayListIterator.java
@@ -1,5 +1,3 @@
-// Author - Zoheb Hasan - 114687894 - CSE214 HW1 - Fall 2022
-package cse214hw1;
 import java.util.Iterator;
 public interface TwoWayListIterator <E> extends Iterator <E>{
     void add(E element);

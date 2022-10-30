7921c48a07f2008a5287dc7e70d5042a3b7a6bef
Zoheb Hasan <106490884+ZohebHasan@users.noreply.github.com>
2022-10-30 16:54:51 -0400

    Update ListAbstractType.java

--- ListAbstractType.java
+++ ListAbstractType.java
@@ -1,5 +1,3 @@
-// Author - Zoheb Hasan - 114687894 - CSE214 HW1 - Fall 2022
-package cse214hw1;
 
 public interface ListAbstractType<E> extends CollectionType<E> {
     void add(int index, E element);
